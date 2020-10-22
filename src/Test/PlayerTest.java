86818cb3a597f470712261b8ecc073c4d83f894a
Roger <roger.argente@e-campus.uab.cat>
2020-10-22 16:41:07 +0200

    Player Test and Class
    Modified Test Player to add new tests and to correct some errors. Created class Player

--- src/Test/PlayerTest.java
+++ src/Test/PlayerTest.java
@@ -4,22 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import main.Object;
+import main.Player;
 
 class PlayerTest {
 
 	@Test
 	void ConstructorTest() {
-		String[] player = {"Facundo","Eucalisto","Gracia", "Marcela23"};
+		String[] player = {"Facundo","Euc@list”","xXx_-Gracia-_xXx", "Marcela23",""};
 
-		Object ply = new Player("Facundo"); 
-		Object ply2 = new Player("Eucalisto");
-		Object ply3 = new Player("Gracia");
-		Object ply4 = new Player("Marcela23");
-
-		assertEquals(player[0],ply.getType());
-		assertEquals(player[1],ply.getType());
-		assertEquals(player[2],ply3.getType());
-		assertEquals(player[3],ply4.getType());
+		Player ply = new Player("Facundo"); 
+		Player ply2 = new Player("Euc@list”");
+		Player ply3 = new Player("xXx_-Gracia-_xXx");
+		Player ply4 = new Player("Marcela23");
+		Player ply5 = new Player("");
+		
+		
+		assertEquals(player[0],ply.getName());
+		assertEquals(player[1],ply2.getName());
+		assertEquals(player[2],ply3.getName());
+		assertEquals(player[3],ply4.getName());
+		assertEquals(player[4],ply5.getName());
 	}
 }
