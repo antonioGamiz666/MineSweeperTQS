7bbb6797fe3843644c40a69ab47e20d3bf6806f8
Roger <roger.argente@e-campus.uab.cat>
2020-11-13 18:26:17 +0100

    RankingPlayer class
    first version of class

--- src/main/RankingPlayer.java
+++ src/main/RankingPlayer.java
@@ -1,5 +1,21 @@
 package main;
 
 public class RankingPlayer {
+	private String name;
+	private int points;
+	
+	public RankingPlayer(String name, int points){
+		this.name = name;
+		this.points = points;
+	}
+	
+	public String getName() {return name;}
+	
+	public void setName(String type) {this.name = name;}
+	
+	public int getPoints() {return points;}
+	
+	public void setPoints(String type) {this.points = points;}
+	
 
 }
