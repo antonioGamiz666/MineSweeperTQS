770c343cf35c5a84cd1d389d50210a05dbbfa368
Roger <roger.argente@e-campus.uab.cat>
2020-11-13 18:47:47 +0100

    rankingTest
    version 1.0 rankingTest

--- src/Test/rankingTest.java
+++ src/Test/rankingTest.java
@@ -4,11 +4,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.Ranking;
+import main.RankingPlayer;
+
 class rankingTest {
 
 	@Test
-	void createRankingPlayerTest() {
-	
+	void addPeopleRankingTest() {
+		RankingPlayer person1 = new RankingPlayer("persona1", 501);
+		RankingPlayer person2 = new RankingPlayer("persona2", 501);
+		RankingPlayer person3 = new RankingPlayer("persona3", 501);
+		RankingPlayer person4 = new RankingPlayer("persona4", 501);
+		RankingPlayer person5 = new RankingPlayer("persona5", 501);
+		RankingPlayer person6 = new RankingPlayer("persona6", 501);
+		RankingPlayer person7 = new RankingPlayer("persona7", 20);
+		RankingPlayer person8 = new RankingPlayer("persona8", 9001);
+		RankingPlayer person9 = new RankingPlayer("persona9", 300);
+		RankingPlayer person10 = new RankingPlayer("persona10", 501);
+		RankingPlayer person11 = new RankingPlayer("persona11", 502);
+		
+		//5 asserts equal
+		assertEquals(person8,getRanking(1));
+		assertEquals(person11,getRanking(2));
+		assertEquals(person1,getRanking(3));
+		assertEquals(person2,getRanking(4));
+		assertEquals(person3,getRanking(5));
 	}
+	
+	
 
 }
