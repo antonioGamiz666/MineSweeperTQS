902f8b7dfb3fecac227d3f15a798c81d57eec9e6
Roger <roger.argente@e-campus.uab.cat>
2020-11-13 18:22:32 +0100

    Ranking Tests
    tests of ranking 1.0

---
+++ src/Test/rankingTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class rankingTest {
+
+	@Test
+	void createRankingPlayerTest() {
+	
+	}
+
+}
