c98b18099bd2bedf0a2225c94be98234ab9a5577
Antonio <antonio.gamiz@e-campus.uab.cat>
2020-11-13 16:21:52 +0100

    Commit correct Test
    Commit correct test from printMenu

--- src/Test/PrinterTest.java
+++ src/Test/PrinterTest.java
@@ -1,5 +1,24 @@
 package Test;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.Printer;
+
+
 public class PrinterTest {
+	
+	@Test
+	void menuTest()
+	{
+		Printer printer = new Printer();
+		
+		printer.printMenu();
+		assertTrue(true);
+		
+	}
+	
 
 }
