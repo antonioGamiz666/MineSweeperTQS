8d60fb0a28c74149277a1273be5ec9758a18b563
Roger <roger.argente@e-campus.uab.cat>
2020-11-13 21:22:15 +0100

    TestRanking update
    updated error

--- src/Test/rankingTest.java
+++ src/Test/rankingTest.java
@@ -5,35 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import main.Ranking;
-import main.RankingPlayer;
 
 class rankingTest {
 
 	@Test
 	void addPeopleRankingTest() {
-		String name = "person1";
-		int points = 501;
+		
+		createRankingPlayer("person1", 501);
+		createRankingPlayer("person2", 501);
+		createRankingPlayer("person3", 501);
+		createRankingPlayer("person4", 501);
+		createRankingPlayer("person5", 501);
+		createRankingPlayer("person6", 501);
+		createRankingPlayer("person7", 20);
+		createRankingPlayer("person8", 9001);
+		createRankingPlayer("person9", 300);
+		createRankingPlayer("person10", 501);
+		createRankingPlayer("person11", 502);
 
-		createRankingPlayer(name, points);
-		/*RankingPlayer person2 = new RankingPlayer("person2", 501);
-		RankingPlayer person3 = new RankingPlayer("person3", 501);
-		RankingPlayer person4 = new RankingPlayer("person4", 501);
-		RankingPlayer person5 = new RankingPlayer("person5", 501);
-		RankingPlayer person6 = new RankingPlayer("person6", 501);
-		RankingPlayer person7 = new RankingPlayer("person7", 20);
-		RankingPlayer person8 = new RankingPlayer("person8", 9001);
-		RankingPlayer person9 = new RankingPlayer("person9", 300);
-		RankingPlayer person10 = new RankingPlayer("person10", 501);
-		RankingPlayer person11 = new RankingPlayer("person11", 502);*/
 		
 		//5 asserts equal
-		assertEquals(name,getRanking(1).getName());
-		/*assertEquals(person11,getRanking(2));
-		assertEquals(person1,getRanking(3));
-		assertEquals(person2,getRanking(4));
-		assertEquals(person3,getRanking(5));*/
+		assertEquals("person8",getRanking(1).getName());
+		assertEquals("person11",getRanking(2).getName());
+		assertEquals("person1",getRanking(3).getName());
+		assertEquals("person2",getRanking(4).getName());
+		assertEquals("person3",getRanking(5).getName());
 	}
-
-
 
 }
