224b5f3c8b2d7e4e9dac54ab9f681dedbc2eb7ad
Roger <roger.argente@e-campus.uab.cat>
2020-11-13 16:14:06 +0100

    Class Board
    First Version of class

--- src/main/Board.java
+++ src/main/Board.java
@@ -2,4 +2,25 @@ package main;
 
 public class Board {
 
+	private int numMines;
+	private int maxX;
+	private int maxY;
+	
+	public Board(int numMines, int maxX,	int maxY){
+		this.numMines = numMines;
+		this.maxX = maxX;
+		this.maxY = maxY;
+	}
+	
+	public int getNumMines() {return numMines;}
+	
+	public void setNumMines(String type) {this.numMines = numMines;}
+	
+	public int getMaxX() {return maxX;}
+	
+	public void setMaxX(String type) {this.maxX = maxX;}
+	
+	public int getMaxY() {return maxY;}
+	
+	public void setMaxY(String type) {this.maxY = maxY;}
 }
