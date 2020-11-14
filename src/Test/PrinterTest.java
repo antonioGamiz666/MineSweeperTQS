4feea2ea3ebb08c97fb67c04d6af5c4e69f21067
Antonio <antonio.gamiz@e-campus.uab.cat>
2020-11-14 13:05:35 +0100

    Commit new Test
    New tests from printBoard()

--- src/Test/PrinterTest.java
+++ src/Test/PrinterTest.java
@@ -10,7 +10,7 @@ import main.Printer;
 
 public class PrinterTest {
 	
-	@Test
+	//@Test
 	void menuTest()
 	{
 		Printer printer = new Printer();
@@ -20,12 +20,31 @@ public class PrinterTest {
 		
 	}
 	
-	@Test boardPrintTest()
+	@Test 
+	void boardPrintTest()
 	{
-		Printer printer = new Printer();
-		 Board board = new mockObjectBoard(2, 3, 3);
 		
+		//printAllChar();
+		Printer printer = new Printer();
+		
+		//Print all board hide
+		Board board = new mockObjectBoard(2, 3, 3);
 		printer.printBoard(board);
+		assertTrue(true);
+		
+		//Print all board not hide
+		Board board1 = new mockObjectBoard(1, 3, 3);
+		printer.printBoard(board1);
+		assertTrue(true);
+	}
+	
+	
+	void printAllChar()
+	{
+		for (int i = 0; i<256; i++)
+		{
+			System.out.println(i +": " + (char)i);
+		}
 	}
 
 }
--- src/Test/mockObjectBoard.java
+++ src/Test/mockObjectBoard.java
@@ -5,11 +5,36 @@ import main.Square;
 public class mockObjectBoard implements Board {
 
 	private Square listSquare[][];
+	private int maxX;
+	private int maxY;
+	
 	public mockObjectBoard(int numMines, int maxX,    int maxY){
 		
 		listSquare = new Square[maxX][maxY];
 		switch (numMines) {
+		
 		case 1:
+			listSquare[0][0] = new Square("mine");
+			listSquare[0][0].setSelected(true);
+			listSquare[0][1] = new Square();
+			listSquare[0][1].setSelected(true);
+			listSquare[0][2] = new Square("flag");
+			listSquare[0][2].setSelected(true);
+			listSquare[1][0] = new Square("empty");
+			listSquare[1][0].setSelected(true);
+			listSquare[1][1] = new Square("mine");
+			listSquare[1][1].setSelected(true);
+			listSquare[1][2] = new Square();
+			listSquare[1][2].setSelected(true);
+			listSquare[2][0] = new Square();
+			listSquare[2][0].setSelected(true);
+			listSquare[2][1] = new Square("mine");
+			listSquare[2][1].setSelected(true);
+			listSquare[2][2] = new Square("number");
+			listSquare[2][2].setSelected(true);
+			break;
+			
+		case 2:
 			listSquare[0][0] = new Square("mine");
 			listSquare[0][1] = new Square();
 			listSquare[0][2] = new Square("flag");
@@ -25,6 +50,8 @@ public class mockObjectBoard implements Board {
 		default:
 			break;
 		}
+		this.maxX = maxX;
+		this.maxY = maxY;
     }
 	
 	
@@ -40,6 +67,22 @@ public class mockObjectBoard implements Board {
 		return listSquare[X][Y];
 
 	}
+
+
+	@Override
+	public int getMaxX() {return maxX;}
+
+
+	@Override
+	public int getMaxY() {return maxY;}
+
+
+	@Override
+	public boolean isSelected(int X, int Y) {return listSquare[X][Y].isSelected();}
+
+
+	@Override
+	public String getTypeObject(int X, int Y) {return listSquare[X][Y].getTypeObject();}
 	
 	
 
