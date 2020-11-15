5a28e7899d4e9861ebc0142af2adf0f14d3615fe
Antonio <antonio.gamiz@e-campus.uab.cat>
2020-11-15 16:50:20 +0100

    Commit 2 test more
    This commit add 2 test more for openSquare

--- src/Test/BoardTest.java
+++ src/Test/BoardTest.java
@@ -48,21 +48,21 @@ class BoardTest {
 		System.out.println("--------------------First test--------------");
 		
 		Board board = new Board(0,5,5);		
-		board.setTypeObject(3, 3, "mine");
-		board.setNumbertest(3, 3);
+		board.setTypeObject(2, 2, "mine");
+		board.setNumbertest(2, 2);
 		
 		printer.printBoard(board);
 		System.out.println("");
 		System.out.println("-------------------------------------------");
 		System.out.println("------------Check wrong number(5,3)--------");
-		assertEquals(0, board.openSquare(5, 3));
+		assertEquals(-1, board.openSquare(5, 3));
 		System.out.println("-------------------------------------------");
 		System.out.println("");
 		
 		System.out.println("");
 		System.out.println("-------------------------------------------");
 		System.out.println("------------Check wrong number(3,5)--------");
-		assertEquals(0, board.openSquare(3, 5));
+		assertEquals(-1, board.openSquare(3, 5));
 		System.out.println("-------------------------------------------");
 		System.out.println("");
 		
@@ -77,11 +77,11 @@ class BoardTest {
 		printer.printBoard(board);		
 		System.out.println("");
 		System.out.println("-------------------------------------------");
-		System.out.println("---------Check open empty Square(1,1)------");
+		System.out.println("---------Check open empty Square(0,0)------");
 		System.out.println("-------------------------------------------");
 		System.out.println("");
 		
-		board.openSquare(1,1);
+		board.openSquare(0,0);
 		
 		printer.printBoard(board);		
 		System.out.println("");
@@ -107,8 +107,53 @@ class BoardTest {
 		System.out.println("-------------------------------------------");
 		System.out.println("");
 		
-		board.openSquare(3,3);
+		board.openSquare(2,2);
+		
 		printer.printBoard(board);
+		System.out.println("");
+		System.out.println("-------------------------------------------");
+		System.out.println("------------------Three test---------------");
+		System.out.println("-------------------------------------------");
+		System.out.println("");
+		
+		board = new Board(0,5,5);
+		board.setTypeObject(2,2, "mine");
+		board.setNumbertest(2,2);
+		
+		printer.printBoard(board);		
+		System.out.println("");
+		System.out.println("-------------------------------------------");
+		System.out.println("-------------Check open a mine-------------");
+		System.out.println("-------------------------------------------");
+		System.out.println("");
+		
+		board.openSquare(2, 2);
+		
+		printer.printBoard(board);
+		System.out.println("");
+		System.out.println("-------------------------------------------");
+		System.out.println("------------------Four test---------------");
+		System.out.println("-------------------------------------------");
+		System.out.println("");
+		
+		board = new Board(0,5,5);
+		board.setTypeObject(2,2, "mine");
+		board.setNumbertest(2,2);
+		board.setTypeObject(1, 2, "flag");
+		board.setTypeObject(1, 0, "flag");
+		board.setTypeObject(0, 1, "flag");
+		
+		printer.printBoard(board);		
+		System.out.println("");
+		System.out.println("-------------------------------------------");
+		System.out.println("-------Check openSquare with flags---------");
+		System.out.println("-------------------------------------------");
+		System.out.println("");
+		
+		board.openSquare(4, 4);
+		
+		printer.printBoard(board);
+		
 		
 		
 	}
