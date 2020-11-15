da88507682e679709d29fdcce51aca206f5cbbf1
Antonio <antonio.gamiz@e-campus.uab.cat>
2020-11-15 23:01:48 +0100

    Commit testCode askMovement()
    This commit add the testCode askMovement()

---
+++ src/Test/GameTest.java
@@ -0,0 +1,23 @@
+package Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import main.Game;
+
+
+public class GameTest {
+	
+	
+	@Test
+	void askMovementTest()
+	{
+		Game game = new Game();
+		
+		game.askMovementTest();
+	}
+	
+	
+
+}
